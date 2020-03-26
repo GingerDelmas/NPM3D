ffa35cfe64a6145c838501a04a1e96930073bf41
Jeffery Durand <Jeffery@Jefferys-MacBook-Air-2.local>
2020-03-26 14:10:25 +0100

    'cloud' class now only takes one ply file + related minor improvements

--- code/main.py
+++ code/main.py
@@ -10,30 +10,38 @@ Jeffery Durand et Ginger Delmas
 
 import numpy as np
 import os, time, pickle
-from ply import write_ply, read_ply
+from ply import read_ply
 
 from sklearn.neighbors import KDTree
 
 class saveable:
-    """ enables subclasses to save and load"""
+    """ 
+        Enables subclasses to save and load. Takes:
+            - save_dir : directory where we will save the cloud for later reuse
+            - save_file : name of the file where we will save the cloud
+    """
     
     def __init__(self, save_dir, save_file):
+        # save the directory, file name, and path
         self.save_dir = save_dir
         self.save_file = save_file
         self.save_path = save_dir + '/' + save_file
     
     
     def save(self, save_path=None):
+        # save the file at the default or custom path
         if save_path is None: 
             save_path = self.save_path
         with open(save_path, 'wb') as f:
             pickle.dump(self.__dict__, f)
             
             
-    def load(self, save_path=None):
+    def load(self, load_if_possible=True, save_path=None):
+        # load a file from the default or custom path
         if save_path is None: 
             save_path = self.save_path
-        if os.path.exists(save_path):
+        # return True if there is a file to load and we want to load it
+        if load_if_possible and os.path.exists(save_path):
             with open(save_path, 'rb') as f:
                 self.__dict__ = pickle.load(f)
             return True
@@ -42,15 +50,17 @@ class saveable:
             
 
 class cloud(saveable):
-    """ basic class for point clouds """
+    """ 
+        Basic class for a point cloud. Takes:
+            - ply_path : the path to the .ply file containing the cloud
+            - save_dir and save_file : see "saveable" class
+            - include_labels : boolean indicating if we should (or can) take labels
+    """
     
-    def __init__(self, save_dir, save_file):
-        super().__init__(save_dir, save_file)
+    def __init__(self, ply_path, save_dir, save_file):
         
-        self.points = np.empty((0, 3))
-        self.labels = np.empty(0, dtype=int)
-        self.cloud_id = np.empty(0, dtype=int)
-        self.trees = {}
+        # call the "saveable" class __init__()
+        super().__init__(save_dir, save_file)
         
         # the set categories of the data
         self.label_names = {0: 'Unclassified',
@@ -60,48 +70,50 @@ class cloud(saveable):
                             4: 'Pedestrians',
                             5: 'Cars',
                             6: 'Vegetation'}
-
+        
+        # save path to ply file
+        self.ply_path = ply_path
+        
+        
+    def fetch_points(self, include_labels: bool):
+        # read the ply file and store content
+        cloud_ply = read_ply(self.ply_path)
+        self.points = np.vstack((cloud_ply['x'], cloud_ply['y'], cloud_ply['z'])).T
+        if include_labels: self.labels = cloud_ply['class']
+        # make the KD Tree
+        self.tree = KDTree(self.points)
+        
 
 class train_cloud(cloud):
-    """ compile and preprocess train data """
+    """ 
+        Basic class for a training point cloud. Takes:
+            - ply_path, save_dir, and save_file: see "cloud" class
+            - load_if_possible : if True, loads the previously saved version if possible
+            - num_points_per_label : number of points to randomly sample per labeled class
+    """
     
-    def __init__(self, train_dir, save_dir, save_file='train_cloud', load_if_possible=True,
+    def __init__(self, ply_path, save_dir, save_file, load_if_possible=True,
                  num_points_per_label=500):
         
-        super().__init__(save_dir, save_file)
+        # call the "cloud" class __init__()
+        super().__init__(ply_path, save_dir, save_file)
         
-        if load_if_possible and not self.load():
-            self.get_train_points(train_dir)
+        # if load() succeeds, skip initializing
+        if not self.load(load_if_possible):
+            # include labels since this is the train set
+            self.fetch_points(include_labels=True)
             self.sample_n_points_per_label(num_points_per_label)
-            self.save()
-        
-        
-    def get_train_points(self, train_dir):
-        # Get all the ply files in data folder
-        ply_files = [f for f in os.listdir(train_dir) if f.endswith('.ply')]
-        
-        self.num_clouds = len(ply_files)
-        
-        # Loop over each training cloud
-        for i, file in enumerate(ply_files):
-
-            # Load Training cloud
-            cloud_ply = read_ply(train_dir + '/' + file)
-            file_points = np.vstack((cloud_ply['x'], cloud_ply['y'], cloud_ply['z'])).T
-            file_labels = cloud_ply['class']
-            
-            # add to the recorded points
-            self.points = np.append(self.points, file_points, axis=0)
-            self.labels = np.append(self.labels, file_labels)
-            self.cloud_id = np.append(self.cloud_id, np.full(len(file_points), i))
-            
-            # make a KDTree
-            self.trees[i] = KDTree(self.points)
         
             
     def sample_n_points_per_label(self, num_points_per_label):
+        """
+            Sample a fixed number of points per labeled class to:
+                1) limit computation time
+                2) avoid bias more common classes
+        """
+        
         # dictionary of sampled points
-        self.samples = {}
+        self.samples_indices = {}
         
         for label in self.label_names.keys():
             
@@ -109,41 +121,41 @@ class train_cloud(cloud):
             if self.label_names[label] == 'Unclassified': continue
             
             # randomly sample points corresponding to the given label
+            # choose as many as possible if there aren't enough points
             label_indices = np.flatnonzero(self.labels == label)
-            assert len(label_indices) > num_points_per_label, 'not enough points per label'
-            sampled_indices = np.random.choice(label_indices, num_points_per_label, replace=False)
+            try:
+                sampled_indices = np.random.choice(label_indices, num_points_per_label, replace=False)
+            except ValueError:
+                sampled_indices = label_indices
+                print("Warning: class '{}' only has {}/{} points".format(
+                    self.label_names[label], len(label_indices), num_points_per_label))
             
             # add to the recorded sampled points
-            self.samples[label] = self.points[sampled_indices]
+            self.samples_indices[label] = sampled_indices
         
-        return self.samples
+        return self.samples_indices
             
     
 class test_cloud(cloud):
-    """ compile and preprocess test data """
+    """ Basic class for a test point cloud """
     
-    def __init__(self, test_path, save_dir, save_file='test_cloud', load_if_possible=True):
+    def __init__(self, ply_path, save_dir, save_file, load_if_possible=True):
         
-        super().__init__(save_dir, save_file)
+        # call the "cloud" class __init__()
+        super().__init__(ply_path, save_dir, save_file)
         
-        if load_if_possible and not self.load():
-            self.get_test_points(test_dir)
-            self.save()
-        
-    def get_test_points(self, test_path):
-        # Load Training cloud
-        cloud_ply = read_ply(test_path)
-        self.points = np.vstack((cloud_ply['x'], cloud_ply['y'], cloud_ply['z'])).T
-        self.cloud_id = np.full(len(self.points), 0)
-        self.trees[0] = KDTree(self.points)
-        
+        # if load_if_possible is True and load() succeeds, skip initializing
+        if not self.load(load_if_possible):
+            # skip labels since this is the test set
+            self.fetch_points(include_labels=False)
+    
         
 class neighborhood:
     """ general class for a point's k nearest neighborhood """
+    
     def __init(self, cloud, indices):
         self.indices = indices
         self.cloud = cloud
-        self.cloud_id = cloud.cloud_id[indices[0]]
         self.k = len(indices)
     
     def points(self):
@@ -155,7 +167,6 @@ class neighborhood_finder(saveable):
     def __init__(self, points, queries, ):
         self.points = points
         self.queries = queries
-        
         
     def k_with_min_entropy(entropy_func, k_min=10, k_max=100):
         for k in range(k_min, k_max):
@@ -191,8 +202,15 @@ if __name__ == '__main__':
     results_dir = '../../NPM3D_local_files/results'
     save_dir = '../../NPM3D_local_files/saves'
     
-    print('Collect Training Set')
+    print('Collect and preprocess training sets')
     t0 = time.time()
-    tc = train_cloud(train_dir, save_dir)
+    
+    ply_files = [f for f in os.listdir(train_dir) if f.endswith('.ply')]
+    
+    for i, file in enumerate(ply_files):
+        tc = train_cloud(train_dir + '/' + file, save_dir, 'train_cloud_{}'.format(i))
+        tc.save()
+        print("Processed {}/{} clouds: {}".format(i + 1, len(ply_files), file))
+        
     t1 = time.time()
     print('Done in %.3fs\n' % (t1 - t0))
